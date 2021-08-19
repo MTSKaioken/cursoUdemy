29b7163bd3c824b40a0468288933b6540cbc874c
Kaio Ruan <Kaio.martins2@aluno.unip.br>
2021-08-19 20:02:50 -0300

    Criação do projeto

---
+++ src/main/java/br/com/spring/cursoUdemy/CursoUdemyApplication.java
@@ -0,0 +1,13 @@
+package br.com.spring.cursoUdemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoUdemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/spring/cursoUdemy/CursoUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spring.cursoUdemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
